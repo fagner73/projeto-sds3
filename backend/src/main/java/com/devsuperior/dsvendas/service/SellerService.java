e7059d4203d1e5c4384e7f12782d61e65ee6a9a4
Fagner Silva <fagnersilva@alu.uern.br>
2021-05-06 16:46:42 -0300

    Pagination

--- backend/src/main/java/com/devsuperior/dsvendas/service/SellerService.java
+++ backend/src/main/java/com/devsuperior/dsvendas/service/SellerService.java
@@ -7,21 +7,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsvendas.dto.SellerDTO;
+
 import com.devsuperior.dsvendas.entities.Seller;
 import com.devsuperior.dsvendas.repositories.SellerRepository;
 
 
 @Service
 public class SellerService {
-	
-	
+ 
 	@Autowired
-	private SellerRepository repository;
+   private SellerRepository repository;
 	
+
+
 	public List<SellerDTO> findAll(){
 	List<Seller> result = repository.findAll();
 	return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
 	}
 	
-
+	
 }
